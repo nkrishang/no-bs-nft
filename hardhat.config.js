84c96b5511b387df935f950e31ac1be3f04de077
nkrishang <kn1457@nyu.edu>
2021-04-08 09:46:07 -0400

    Added eth dependencies, contracts and compilation scripts

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
+
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const ROPSTEN_PRIVATE_KEY = process.env.ROPSTEN_PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.3",
+  networks: {
+    ropsten: {
+      url: `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${ROPSTEN_PRIVATE_KEY}`]
+    }
+  },
+
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+};
