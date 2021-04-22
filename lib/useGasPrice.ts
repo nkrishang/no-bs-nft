3c0c0cc8f829d7c74d1309257c91ed8981f86a87
nkrishang <kn1457@nyu.edu>
2021-04-22 16:04:30 -0400

    update cost display

--- lib/useGasPrice.ts
+++ lib/useGasPrice.ts
@@ -1,32 +1,32 @@
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
-export default function useGasPrice(chainId: number): any {
+export default function useGasPrice(chainId: number = 1): any {
 
   const [gasPrice, setGasPrice] = useState<string>('');
-  // const [ethPrice, setEthPrice] = useState<string>('');
-  // const [maticPrice, setMaticPrice] = useState<string>('');
-  // const [costEstimates, setCostEstimates] = useState<any>('');
+  const [ethPrice, setEthPrice] = useState<string>('');
+  const [maticPrice, setMaticPrice] = useState<string>('');
+  const [costEstimates, setCostEstimates] = useState<any>('');
 
   const uploadTransactionGas = useRef(300000);
   const deployTransactionGas = useRef(6550000)
   
   useEffect(() => {
 
-    // const getGasPrice = async () => {
-    //   const res = await axios.get(
-    //     `https://ethgasstation.info/api/ethgasAPI.json?api-key=${process.env.NEXT_PUBLIC_GASPRICE_API_KEY}`
-    //   )
+    const getGasPrice = async () => {
+      const res = await axios.get(
+        `https://ethgasstation.info/api/ethgasAPI.json?api-key=${process.env.NEXT_PUBLIC_GASPRICE_API_KEY}`
+      )
 
-    //   if(res.status == 200) {
-    //     const { data } = res
-    //     console.log("Fast gas price in gwei: ", data.fast / 10);
+      if(res.status == 200) {
+        const { data } = res
+        console.log("Fast gas price in gwei: ", data.fast / 10);
 
-    //     setGasPrice((data.fast / 10).toString());
-    //   } else {
-    //     console.log("gas price response failed: ", res);
-    //   }
-    // }
+        setGasPrice((data.fast / 10).toString());
+      } else {
+        console.log("gas price response failed: ", res);
+      }
+    }
     
     // TEMPORARY -- hard coded gas prices.
     switch (chainId) {
@@ -35,18 +35,118 @@ export default function useGasPrice(chainId: number): any {
         setGasPrice('150');
         break;
       case 3:
-        setGasPrice('20');
+        setGasPrice('10');
         break;
       case 80001:
-        setGasPrice('200')
+        setGasPrice('10')
         break;
       case 137:
-        setGasPrice('100');
+        setGasPrice('10');
         break;
       default:
         break;
     }
   }, [chainId])
+
+  const getEthPrice = async () => {
+      
+    try {
+      const res = await axios.get(
+        `https://rest.coinapi.io/v1/exchangerate/ETH/USD?apikey=${process.env.NEXT_PUBLIC_COINAPI_KEY}`
+      )
+      
+      if(res.status == 200) {
+        const { data } = res;
+        console.log("ETH PRICE: ", (data.rate).toString())
+        return parseFloat((data.rate).toString())
+      } else {
+        console.log("Coin API for eth failed: ", res);
+        return 0
+      }
+    } catch(err) {
+      console.log(err)
+      return 0
+    }      
+  }
+
+  const getMaticPrice = async () => {
+      
+    try {
+      const res = await axios.get(
+        `https://rest.coinapi.io/v1/exchangerate/MATIC/USD?apikey=${process.env.NEXT_PUBLIC_COINAPI_KEY}`
+      )
+
+      if(res.status == 200) {          
+        const { data } = res;
+        console.log("MATIC PRICE: ", (data.rate).toString())
+        return parseFloat((data.rate).toString());
+      } else {
+        console.log("Coin API for matic failed: ", res);
+        return 0
+      }
+    } catch(err) {
+      console.log(err)
+      return 0
+    }      
+  }
+
+  const getCostEstimates = (price: number) => {
+    const uploadCost: number = (
+      ( (uploadTransactionGas.current) * price ) / 10**9
+    )
+
+    const deployCost: number = (
+      ( (deployTransactionGas.current) * parseInt(gasPrice) * price ) / 10**9
+    )
+
+    const uploadCostDecimals = (uploadCost.toString()).split('.')
+    const formattedUploadCost = uploadCostDecimals[0] + '.' + (
+      uploadCostDecimals[1].length > 3 ? uploadCostDecimals[1].slice(0,6) : uploadCostDecimals[1]
+    );
+    
+    const deployCostEthDecimals = (deployCost.toString()).split('.')
+    
+    const formattedDeployCost = deployCostEthDecimals[0] + '.' + (
+      deployCostEthDecimals[1].length > 3 ? deployCostEthDecimals[1].slice(0,3) : deployCostEthDecimals[1]
+    );
+
+    return {
+      uploadCost: formattedUploadCost,
+      deployCost: formattedDeployCost
+    }
+  }
+
+  const getCost = async () => {
+    const eth: number = await getEthPrice();
+    const matic: number = await getMaticPrice();
+    console.log(`eth: ${eth} matic ${matic}`)
+    if (eth == 0 || matic == 0) {
+      return {
+        uploadCost: {
+          eth: 0,
+          matic: 0
+        },
+        deployCost: {
+          eth: 0,
+          matic: 0
+        }
+      }
+    }
+
+    const ethCosts = getCostEstimates(eth);
+    const maticCosts = getCostEstimates(matic);
+
+    return {
+      uploadCost: {
+        eth: ethCosts.uploadCost,
+        matic: maticCosts.uploadCost
+      },
+      deployCost: {
+        eth: ethCosts.deployCost,
+        matic: maticCosts.deployCost
+      }
+    }
+  }
 
   // useEffect(() => {
 
@@ -108,23 +208,23 @@ export default function useGasPrice(chainId: number): any {
 
   // useEffect(() => {
     
-  //   if(chainId == 3 || chainId == 1) {
-  //     if(ethPrice && gasPrice) {
-  //       const costOfUploadTransaction: number = (
-  //         ( (uploadTransactionGas.current) * parseInt(gasPrice) * parseInt(ethPrice) ) / 10**9
-  //       )
+    // if(chainId == 3 || chainId == 1) {
+    //   if(ethPrice && gasPrice) {
+    //     const costOfUploadTransaction: number = (
+    //       ( (uploadTransactionGas.current) * parseInt(gasPrice) * parseInt(ethPrice) ) / 10**9
+    //     )
 
-  //       const costOfDeployTransaction: number = (
-  //         ( (deployTransactionGas.current) * parseInt(gasPrice) * parseInt(ethPrice) ) / 10**9
-  //       )
+    //     const costOfDeployTransaction: number = (
+    //       ( (deployTransactionGas.current) * parseInt(gasPrice) * parseInt(ethPrice) ) / 10**9
+    //     )
 
-  //       console.log("rETH price: ", ethPrice)
-  //       console.log("cost of deploy tx eth: ", costOfDeployTransaction.toString())
-  //       setCostEstimates({
-  //         uploadTransaction: parseInt(costOfUploadTransaction.toString()).toString(),
-  //         deployTransaction: parseInt(costOfDeployTransaction.toString()).toString()
-  //       })
-  //     }
+    //     console.log("rETH price: ", ethPrice)
+    //     console.log("cost of deploy tx eth: ", costOfDeployTransaction.toString())
+    //     setCostEstimates({
+    //       uploadTransaction: parseInt(costOfUploadTransaction.toString()).toString(),
+    //       deployTransaction: parseInt(costOfDeployTransaction.toString()).toString()
+    //     })
+    //   }
   //   } else if (chainId == 137 || chainId == 80001) {
   //     if(maticPrice && gasPrice) {
   //       const costOfUploadTransaction: number = (
@@ -150,7 +250,8 @@ export default function useGasPrice(chainId: number): any {
   //   uploadTransaction: uploadTransactionGas.current
   // }}
 
-  return {gasPrice, gasEstimates: {
-    uploadTransaction: uploadTransactionGas.current
+  return {getCost, gasPrice, gasEstimates: {
+    uploadTransaction: uploadTransactionGas.current,
+    deployTransactionGas: deployTransactionGas.current
   }}
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -10,10 +10,34 @@ import {
 import { InfoOutlineIcon, ArrowRightIcon } from "@chakra-ui/icons";
 
 import { ContentWrapper } from 'components/ContentWrapper';
+import useGasPrice from 'lib/useGasPrice';
+import { useEffect, useState } from 'react';
 
 
 export default function App(): JSX.Element {
   
+  // const { getCost } = useGasPrice();
+
+  const [uploadCost, setUploadCost] = useState<any>('')
+  const [deployCost, setdeployCost] = useState<any>('')
+
+  // useEffect(() => {
+  //   const getPrices = async () => {
+  //     const { uploadCost, deployCost } = await getCost()
+
+  //     if(parseFloat(uploadCost.eth) > 0 && parseFloat(uploadCost.matic) > 0) {
+  //       setUploadCost(uploadCost);
+  //     }
+
+  //     if(parseFloat(deployCost.eth) > 0 && parseFloat(deployCost.matic) > 0) {
+  //       setdeployCost(deployCost)
+  //     }
+
+  //     console.log(uploadCost, deployCost)
+  //   }
+
+  //   getPrices();
+  // }, [])
 
   return (
     <>      
@@ -32,8 +56,26 @@ export default function App(): JSX.Element {
                 </p>
               </Center>
 
+              <Center>              
+                <Stack>
+                  <p className="text-lg text-gray-800 font-light text-center mb-2">
+                    We're on Ethereum Mainnet and Matic (Polygon)
+                  </p>                                   
+                  <span className="text-center">                
+                    Create an NFT collection on Matic (est.): <span className="text-green-600 font-normal">${
+                      deployCost ? deployCost.matic : '0.3'
+                    }</span>
+                  </span>
+                  <span className="text-center">                  
+                    Upload 1 media file to an NFT collection on Matic (est.): <span className="text-green-600 font-normal">${
+                      uploadCost ? uploadCost.matic : '0.0001'
+                    }</span>
+                  </span>
+                </Stack>
+              </Center>
+
               <Center>
-                <div className="mb-16">
+                <div className="mb-16 mt-8">
                   <Link href="/create">
                     <Button variant="link" rightIcon={<ArrowRightIcon boxSize="0.75em"/>}>                    
                       <p className="text-gray-700 font-light text-lg">
